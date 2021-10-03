7b98142b8f0adbc3f5b575a746ba552c1e0d7385
brookecalhoun <86681030+brookecalhoun@users.noreply.github.com>
2021-10-03 16:28:30 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,27 @@ function App() {
       <header className="App-header">
         {" "}
         <h3>Welcome to Brooke's Portfolio</h3>{" "}
-        <a href="https://github.com/brookecalhoun">
-          <img
-            src={`${process.env.PUBLIC_URL}/github.png`}
-            className="App-logo"
-            alt="logo"
-          />{" "}
-        </a>
+        <div className="img">
+          <a href="https://github.com/brookecalhoun">
+            <img
+              src={`${process.env.PUBLIC_URL}/github.png`}
+              className="App-logo"
+              alt="logo"
+            />{" "}
+          </a> &nbsp;
+          <a href="https://www.linkedin.com/in/brooke-calhoun/">
+            <img
+              src={`${process.env.PUBLIC_URL}/linkedin.png`}
+              className="App-logo"
+              alt="logo"
+            />{" "}
+          </a>
+        </div>
         <br />
-        <p>Site under way! Click the symbol to checkout my GitHub in the meantime!</p>{" "}
+        <p>
+          Site under way! Click the symbol to checkout my GitHub or LinkedIn in
+          the meantime!
+        </p>{" "}
       </header>{" "}
     </div>
   );
