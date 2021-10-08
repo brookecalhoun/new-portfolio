ec05ae87f618e249a5e1530356e6eb3700e5bedf
brookecalhoun <86681030+brookecalhoun@users.noreply.github.com>
2021-10-07 19:19:32 -0700

    styling and format adjustment

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import "./App.css";
+import Name from '../src/assets/name.png'
 function App() {
   return (
-    <div className="App">
-      {" "}
-      <header className="App-header">
-        {" "}
-        <h3>Welcome to Brooke's Portfolio</h3>{" "}
-        <div className="img">
-          <a href="https://github.com/brookecalhoun">
-            <img
-              src={`${process.env.PUBLIC_URL}/github.png`}
-              className="App-logo"
-              alt="logo"
-            />{" "}
-          </a> &nbsp;
-          <a href="https://www.linkedin.com/in/brooke-calhoun/">
-            <img
-              src={`${process.env.PUBLIC_URL}/linkedin.png`}
-              className="App-logo"
-              alt="logo"
-            />{" "}
-          </a>
-        </div>
-        <br />
-        <p>
-          Site under way! Click the symbol to checkout my GitHub or LinkedIn in
-          the meantime!
-        </p>{" "}
-      </header>{" "}
+
+    <div className='App'>
+      {/* <img className='name' src={Name}/> */}
+      <h1 className='name'> Hi I'm Brooke</h1>
     </div>
   );
 }
