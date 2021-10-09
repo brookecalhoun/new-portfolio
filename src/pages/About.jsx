4ca384b242c8f9cf42697690ca68f67ecf054d60
brookecalhoun <86681030+brookecalhoun@users.noreply.github.com>
2021-10-09 14:45:18 -0700

    fixed formatting

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
-function About(){
-    return(
-        <div className='about'>
-            <h1>About me</h1>
-        </div>
-    )
+function About() {
+  return (
+    <div className="about">
+      <h1 className="about-title">About me</h1>
+      <div className='about-description'></div>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-import Projects from "../pages/Projects";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
-import Resume from "../pages/Resume";
 
 function Homepage() {
   return (
     <div className="homepage">
-        <Projects />
       <div className="intro">
         <h1 className="name"> Hi, I'm Brooke.</h1>
         <p className="tagline">Looking to create code that makes a difference.</p>
       </div>
       <div className="tabs">
-        <About />
-        <Contact />
-        <Resume />
       </div>
     </div>
   );
