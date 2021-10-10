07f0c5198dc139908b9423cd6480a603a17cde97
brookecalhoun <86681030+brookecalhoun@users.noreply.github.com>
2021-10-09 23:09:16 -0700

    styling

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,6 +5,7 @@ import Koru from "../assets/koru.mov";
 import Pokemon from "../assets/pokemon.mov";
 import Ehub from "../assets/ehub.mov";
 import Modal from "react-bootstrap/Modal";
+import Me from "../assets/me.jpg";
 
 function Homepage() {
   const [show, setShow] = useState(false);
@@ -25,6 +26,7 @@ function Homepage() {
           Looking to create code that makes a difference.
         </p>
       </div>
+
       <div className="projects" id="projects">
         <h2 className="project-title"> Projects. </h2>
         <div className="videos">
@@ -32,7 +34,7 @@ function Homepage() {
             <h3 className="koru-title">Koru </h3>
             <video controls src={Koru} className="koru-video" />
             <Button
-              variant="primary"
+              variant="danger"
               onClick={handleShow}
               className="koru-button"
             >
@@ -62,7 +64,7 @@ function Homepage() {
             <h3 className="pokemon-title">Pokemon Tamogatchi </h3>
             <video controls src={Pokemon} className="pokemon-video" />
             <Button
-              variant="primary"
+              variant="danger"
               onClick={handleShow2}
               className="poke-button"
             >
@@ -92,7 +94,7 @@ function Homepage() {
             <h3 className="ehub-title">Ehub </h3>
             <video controls src={Ehub} className="ehub-video" />
             <Button
-              variant="primary"
+              variant="danger"
               onClick={handleShow3}
               className="ehub-button"
             >
@@ -113,6 +115,68 @@ function Homepage() {
               </Modal.Footer>
             </Modal>
           </div>
+        </div>
+      </div>
+
+      <div className="about" id="about">
+        <h2 className="about-title"> About Me. </h2>
+        <div className="overlay1">
+          <div className="overlay2">
+            <div className="overlay3">
+              <img src={Me} className="me" />
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="about-description">
+          <div className="skill-list">
+            <div className="css-wrap">
+              <h3 className="css-name">CSS</h3>
+              <img
+                className="css"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+              />
+            </div>
+            <div className="js-wrap">
+              <h3 className="js-name">JavaScript</h3>
+              <img
+                className="js"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+              />
+            </div>
+            <div className="html-wrap">
+              <h3 className="html-name">HTML</h3>
+              <img
+                className="html"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+              />
+            </div>
+            <div className="python-wrap">
+              <h3 className="python-name">Python</h3>
+              <img
+                className="python"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+              />
+            </div>
+            <div className="react-wrap">
+              <h3 className="react-name">React</h3>
+              <img
+                className="react"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+              />
+            </div>
+            <div className="mongo-wrap">
+              <h3 className="mongo-name">MongoDB</h3>
+              <img
+                className="mongo"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
+              />
+            </div>
+          </div>
+          <p className="about-me">
+            I'm a software engineer specializing in front-end development. My
+            passions are innovation, responsive design, and accessibility.{" "}
+          </p>
         </div>
       </div>
     </div>
